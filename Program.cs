fa6f8079c9cc3d02e35a834b207785656b186d6c
Igor <eltery@mail.ru>
2023-01-03 18:56:06 +0300

    Numbers from N to 1 done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//Задача 64: Задайте значение N. Напишите программу,
+//которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+/*
+void ShowNums(int number)
+{
+    Console.Write(number + " ");
+    if(number > 1) ShowNums(number - 1);
+    
+}
+Console.Write("Input number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+ShowNums(userNumber);
+*/
+
