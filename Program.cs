01dc1f5d52e9353016a93d516eb8ccd3173e3aaf
Igor <eltery@mail.ru>
2023-01-08 21:10:17 +0300

    Sum of numbers done

--- Program.cs
+++ Program.cs
@@ -17,24 +17,21 @@ ShowNums(userNumber);
 /*
 int SumOfNumbers(int number1, int number2)
 {
-    
-    if(number1 < number2 + 1) return number1  + SumOfNumbers(number1 + 1, number2);    //проверка условия, чтобы суммирование закончилось на number2, а не на предыдущем числе
-    //if(number1 < number2) return number1 + number2 + SumOfNumbers(number1 + 1, number2);  //попытка закончить вызов рекурсии на числе number2 - 1, прибавив его вне рекурсии. Результат вообще не понятно откуда берётся. Не понимаю что с чем складывается
-        
-    //if(number2 < number1 + 1) return number2 + SumOfNumbers(number1, number2 + 1);
-
-    return 0;
-    
+    if (number1 == number2) return number1;
+    else 
+        if(number1 > number2) return number1 + SumOfNumbers(number1 - 1, number2);
+        else return number2 + SumOfNumbers(number1, number2 - 1);
 }
 
 Console.Write("Введите первое число: ");
 int userNumber1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int userNumber2 = Convert.ToInt32(Console.ReadLine());
-int result = SumOfNumbers(userNumber1, userNumber2);
-Console.WriteLine($"Сумма числел между {userNumber1} и {userNumber2} равна {result} ");
 
+int sum = SumOfNumbers(userNumber1, userNumber2);
+Console.WriteLine($"Сумма чисел от {userNumber1} до {userNumber2} равна: {sum}");
 */
+
 
 //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
 //Даны два неотрицательных числа m и n.
